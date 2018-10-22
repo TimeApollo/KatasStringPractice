efb48acda9731d532c5dc26ade2ce8d8be8f3189
TimeApollo <arnjcksn0845@yahoo.com>
2018-10-22 13:54:04 -0400

    more katas strings practice in python

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def to_camel_case( word ):
+    index = word.find('_')
+
+def main():
+    print to_camel_case("the-stealth-warrior")
+
+if __name__ == '__main__':
+    main()
